87ee7b4cd642bfce75096fd6c26e314225ec117c
sean@seanbdurkin.id.au <sean@seanbdurkin.id.au@de69388f-7182-a270-cd33-689644765ec3>
2015-02-25 03:44:10 +0000

    v3.6.1 Support XE7

---
+++ packages/XE7/LockBox_DUnit_Tests.dpr
@@ -0,0 +1,23 @@
+program LockBox_DUnit_Tests;
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  uLockBox_CipherTestCases in '..\..\unit-tests\dunit\uLockBox_CipherTestCases.pas',
+  uLockBox_HugeCardinalTestCases in '..\..\unit-tests\dunit\uLockBox_HugeCardinalTestCases.pas',
+  uLockBox_RSA_TestCases in '..\..\unit-tests\dunit\uLockBox_RSA_TestCases.pas',
+  uLockBox_Signatory_TestCases in '..\..\unit-tests\dunit\uLockBox_Signatory_TestCases.pas',
+  uLockBox_Hashes in '..\..\unit-tests\dunit\uLockBox_Hashes.pas',
+  TPLB3.SVN_Keywords in '..\..\run\TPLB3.SVN_Keywords.pas',
+  uLockBox_OpenSSLTestCases in '..\..\unit-tests\dunit\uLockBox_OpenSSLTestCases.pas',
+  TPLB3.OpenSSL in '..\..\run\TPLB3.OpenSSL.pas',
+  uLockBox_TestCases in '..\..\unit-tests\dunit\uLockBox_TestCases.pas',
+  TPLB3.Compatibility in '..\..\run\TPLB3.Compatibility.pas';
+
+{$R *.res}
+
+begin
+Application.Initialize;
+GUITestRunner.RunRegisteredTests
+end.
